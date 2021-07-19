51cfb4da164018013799c0563b38763dedcfd04f
Daniel Simpson <daniel_simpson1988@hotmail.com>
2021-07-19 19:48:12 +1000

    Use async

--- src/functions/forwardVoice.ts
+++ src/functions/forwardVoice.ts
@@ -2,15 +2,13 @@ import "@twilio-labs/serverless-runtime-types";
 
 import { CallHandler } from "../twilio";
 
-export const handler: CallHandler = function (context, event, callback) {
+export const handler: CallHandler = async function (context, event, callback) {
 
   const from = event.From && event.From.length > 0 ? event.From : context.TWILIO_PHONE;
   if(from !== context.TWILIO_PHONE) {
     // TODO: v3: add caller ID to incoming calls
 
-    const from = event.From && event.From.length > 0 ? event.From : context.TWILIO_PHONE;
-
-    context
+    await context
       .getTwilioClient()
       .messages.create({
         from,
